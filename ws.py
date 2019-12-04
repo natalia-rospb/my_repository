3026be90336927aef9473c90a861d8f2896a5ba5
natalia-rospb <44818682+natalia-rospb@users.noreply.github.com>
2019-12-05 02:18:10 +0300

    Add files via upload

--- ws.py
+++ ws.py
@@ -25,7 +25,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                             <br>
                             <br>
                             <input type="limit" name="limit" placeholder = "docs limit">
-                            <input type="offset" name="offset" placeholder = "docs offset">
+                            <input type="hidden" name="offset" value = "0">
                         </form>
                     </body>
                 </html>
@@ -43,21 +43,25 @@ class RequestHandler(BaseHTTPRequestHandler):
         limit = form.getvalue("limit")
         if not limit:
             limit = 5
-        elif int(limit) < 0:
-            limit = 0
-        else: limit = int(limit)
+        try:
+            limit = int(limit)
+            if limit < 0:
+                limit = 0
+        except ValueError:
+            self.wfile.write(bytes("Wrong query", encoding="UTF-8"))
+            
         offset = form.getvalue("offset")
-        if not int(offset):
+        if not offset:
             offset = 0
         elif int(offset) < 0:
             offset = 0
-        else: offset = int(offset)
-
+        else:
+            offset = int(offset)
         docslimoff = [0] * limit
         for i in range(limit):
                 docslimoff[i] = [3,0]
         searchresult = self.server.search_engine.lim_off_context_window_search(tokenquery,
-                                                    limit, offset, docslimoff)
+                                                    limit, 0, docslimoff)
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
@@ -72,35 +76,68 @@ class RequestHandler(BaseHTTPRequestHandler):
                                 <input type="submit" value="Search">
                                 <br>
                                 <br>
-                                <input type="limit" name="limit" value = "%d">
-                                <input type="offset" name="offset" value = "%d">
-                                <hr />
-                            <ol>
-                            """ % (tokenquery, limit, offset), encoding="utf-8"))
+                                <input type="number" name="limit" value = "%d">
+                                <br>
+                                <br>
+                                <input type="submit" name="action" value = "Beginning">
+                            """ % (tokenquery, limit), encoding="utf-8"))
             # limit - how many files to show on the page at once
-            # offset - the number of an element from which to show citations
+            # offset - the number of a document from which to show citations
             docnumber = 0
             sortedkeys = list(searchresult.keys())
             sortedkeys.sort()
+
+            actiontype = str(form.getvalue("action"))
+            if actiontype == "Beginning":
+                offset = 0
+            elif actiontype == "Forward":
+                offset = offset + limit
+            elif actiontype == "Back":
+                offset = offset - limit
+
+            if offset == 0:
+                self.wfile.write(bytes("""<input type="submit" name="action" value = "Back" disabled>""", encoding="utf-8"))
+            else:
+                self.wfile.write(bytes("""<input type="submit" name="action" value = "Back"> """, encoding="utf-8"))
+            nextpagesearchresult = self.server.search_engine.lim_off_context_window_search(tokenquery,
+                                                    limit, offset+limit, docslimoff)
+            if (len(nextpagesearchresult.keys()) > 0):
+                self.wfile.write(bytes("""<input type="submit" name="action" value = "Forward"> 
+                            <ol>
+                            <hr />""", encoding="utf-8"))
+            else:
+                self.wfile.write(bytes("""<input type="submit" name="action" value = "Forward" disabled>
+                            <ol>
+                            <hr />""", encoding="utf-8"))
+                               
             for doc in sortedkeys:
-                
                 # getting doclimit and docoffset for a particular document
                 doclimit = form.getvalue("doc%dlimit" % docnumber)
                 if not doclimit:
                     doclimit = 3
-                elif int(doclimit) < 0:
+                elif round(int(doclimit)) < 0:
                     doclimit = 0
-                else: doclimit = int(doclimit)
+                else: doclimit = round(int(doclimit))
                 docoffset = form.getvalue("doc%doffset" % docnumber)
                 if not docoffset:
                     docoffset = 0
                 elif int(docoffset) < 0:
                     docoffset = 0
                 else: docoffset = int(docoffset)
+
+                docactiontype = str(form.getvalue("action%d" % docnumber))
+                if docactiontype == "Beginning":
+                    docoffset = 0
+                elif docactiontype == "Forward":
+                    docoffset = docoffset + doclimit
+                elif docactiontype == "Back":
+                    docoffset = docoffset - doclimit
+                
                 docslimoff[docnumber] = [doclimit, docoffset]
                 docnumber += 1
+            
             searchresultlimoff = self.server.search_engine.lim_off_context_window_search(tokenquery,
-                                                limit, offset, docslimoff)
+                                             limit, offset, docslimoff)
             sortedkeyslimoff = list(searchresultlimoff.keys())
             sortedkeyslimoff.sort()
             for x, doc in enumerate(sortedkeyslimoff):
@@ -110,15 +147,30 @@ class RequestHandler(BaseHTTPRequestHandler):
                     self.wfile.write(bytes("<li><p>%s</p></li>" % cw, encoding="UTF-8"))
                 self.wfile.write(bytes("</ul>", encoding="utf-8"))
                 self.wfile.write(bytes("""
-                    <input type="doc%dlimit" name="doc%dlimit" placeholder = "cit.limit in the doc" value = "%d">
-                    <input type="doc%doffset" name="doc%doffset" placeholder = "cit.offset in the doc" value = "%d">
-                        """ % (x, x, docslimoff[x][0], x, x, docslimoff[x][1]), encoding="utf-8"))
+                    <input type="number" name="doc%dlimit" placeholder = "cit.limit in the doc" value = "%d">
+                    <input type="hidden" name="doc%doffset" placeholder = "cit.offset in the doc" value = "%d">
+                    <br>
+                    <input type="submit" name="action" value = "Beginning">
+                    """ % (x, docslimoff[x][0], x, docslimoff[x][1]), encoding="utf-8"))
+                if docslimoff[x][1] == 0:
+                    self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Back" disabled>""" % x, encoding="utf-8"))
+                else:
+                    self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Back"> """ % x, encoding="utf-8"))
+                intermeddoclimoff = docslimoff
+                intermeddoclimoff[x][1] = intermeddoclimoff[x][1] + intermeddoclimoff[x][0]
+                nextpagesearchresult = self.server.search_engine.lim_off_context_window_search(tokenquery,
+                                                        limit, offset, intermeddoclimoff)
+                if (len(nextpagesearchresult.keys()) > 0 and intermeddoclimoff[x][0] != 0):
+                    self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Forward">""" % x, encoding="utf-8"))
+                else:
+                    self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Forward" disabled>""" % x, encoding="utf-8"))
                 
-            self.wfile.write(bytes("""</form>
+            self.wfile.write(bytes("""<input type="hidden" name="offset" value = "%d">
+                                </form>
                             </ol>
                         </body>
                     </html>
-                    """, encoding="utf-8"))
+                    """ % offset, encoding="utf-8"))
 
         
 def main():
