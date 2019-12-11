9b83d646d6d1c9c4a131a010e052c084952ec207
natalia-rospb <44818682+natalia-rospb@users.noreply.github.com>
2019-12-11 21:32:15 +0300

    Add files via upload

--- ws.py
+++ ws.py
@@ -24,7 +24,7 @@ class RequestHandler(BaseHTTPRequestHandler):
                             <input type="submit" value="Search">
                             <br>
                             <br>
-                            <input type="limit" name="limit" placeholder = "docs limit">
+                            <input type="number" name="limit" placeholder = "docs limit">
                             <input type="hidden" name="offset" value = "0">
                         </form>
                     </body>
@@ -36,10 +36,13 @@ class RequestHandler(BaseHTTPRequestHandler):
         '''
         Enables to search in database and see the results as an ordered list of contexts.
         '''
+        print("new")
         form = cgi.FieldStorage(fp=self.rfile, headers=self.headers,
                         environ={'REQUEST_METHOD': 'POST'})
-
         tokenquery = str(form.getvalue("query"))
+
+        # limit - how many files to show on the page at once
+        # offset - the number of a document from which to show citations
         limit = form.getvalue("limit")
         if not limit:
             limit = 5
@@ -49,7 +52,6 @@ class RequestHandler(BaseHTTPRequestHandler):
                 limit = 0
         except ValueError:
             self.wfile.write(bytes("Wrong query", encoding="UTF-8"))
-            
         offset = form.getvalue("offset")
         if not offset:
             offset = 0
@@ -57,15 +59,60 @@ class RequestHandler(BaseHTTPRequestHandler):
             offset = 0
         else:
             offset = int(offset)
-        docslimoff = [0] * limit
+        
+        # getting doclimit and docoffset for a particular document
+        docslimoff = []
         for i in range(limit):
-                docslimoff[i] = [3,0]
-        searchresult = self.server.search_engine.lim_off_context_window_search(tokenquery,
-                                                    limit, 0, docslimoff)
+            doclimit = form.getvalue("doc%dlimit" % i)
+            if not doclimit:
+                doclimit = 3
+            elif round(int(doclimit)) < 0:
+                    doclimit = 0
+            else: doclimit = int(doclimit)
+            docoffset = form.getvalue("doc%doffset" % i)
+            if not docoffset:
+                docoffset = 0
+            elif int(docoffset) < 0:
+                docoffset = 0
+            else: docoffset = int(docoffset)
+            docslimoff.append([doclimit, docoffset])
+
+        # necessary for checking the existence of results for the next page
+        # if the results are confined to the current page, the button "Forward" will be disabled
+        extlimit = limit + 1
+        extdoclimoff = []
+        for item in docslimoff:
+            extdoclimoff.append(item)
+        extdoclimoff.append([3,0])
+        for i in range(extlimit):
+            extdoclimoff[i][0] += 1
+        
+        # getting the action for documents in a whole 
+        actiontype = str(form.getvalue("action"))
+        if actiontype == "Beginning":
+            offset = 0
+        elif actiontype == "Forward":
+            offset = offset + limit
+        elif actiontype == "Back":
+            offset = offset - limit
+
+        # getting the action for each document
+        for i in range(limit):
+            docactiontype = str(form.getvalue("action%d" % i))
+            if docactiontype == "Beginning":
+                extdoclimoff[i][1] = 0
+            elif docactiontype == "Forward":
+                extdoclimoff[i][1] = extdoclimoff[i][1] + extdoclimoff[i][0] - 1
+            elif docactiontype == "Back":
+                extdoclimoff[i][1] = extdoclimoff[i][1] - extdoclimoff[i][0] + 1
+            extdoclimoff[i] = [extdoclimoff[i][0], extdoclimoff[i][1]]
+        
+        extsearchresult = self.server.search_engine.lim_off_context_window_search(tokenquery,
+                                                    extlimit, offset, extdoclimoff)
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
-        if not searchresult:
+        if not extsearchresult:
             self.wfile.write(bytes("Nothing found", encoding="UTF-8"))
         else:
             self.wfile.write(bytes("""
@@ -78,93 +125,50 @@ class RequestHandler(BaseHTTPRequestHandler):
                                 <br>
                                 <input type="number" name="limit" value = "%d">
                                 <br>
-                                <br>
                                 <input type="submit" name="action" value = "Beginning">
                             """ % (tokenquery, limit), encoding="utf-8"))
-            # limit - how many files to show on the page at once
-            # offset - the number of a document from which to show citations
-            docnumber = 0
-            sortedkeys = list(searchresult.keys())
+            sortedkeys = list(extsearchresult.keys())
             sortedkeys.sort()
-
-            actiontype = str(form.getvalue("action"))
-            if actiontype == "Beginning":
-                offset = 0
-            elif actiontype == "Forward":
-                offset = offset + limit
-            elif actiontype == "Back":
-                offset = offset - limit
-
+            try:
+                sortedkeys.pop(limit)
+            except IndexError:
+                pass
             if offset == 0:
                 self.wfile.write(bytes("""<input type="submit" name="action" value = "Back" disabled>""", encoding="utf-8"))
             else:
                 self.wfile.write(bytes("""<input type="submit" name="action" value = "Back"> """, encoding="utf-8"))
-            nextpagesearchresult = self.server.search_engine.lim_off_context_window_search(tokenquery,
-                                                    limit, offset+limit, docslimoff)
-            if (len(nextpagesearchresult.keys()) > 0):
-                self.wfile.write(bytes("""<input type="submit" name="action" value = "Forward"> 
+            if (len(extsearchresult.keys()) < extlimit):
+                self.wfile.write(bytes("""<input type="submit" name="action" value = "Forward" disabled> 
                             <ol>
                             <hr />""", encoding="utf-8"))
             else:
-                self.wfile.write(bytes("""<input type="submit" name="action" value = "Forward" disabled>
+                self.wfile.write(bytes("""<input type="submit" name="action" value = "Forward">
                             <ol>
                             <hr />""", encoding="utf-8"))
-                               
-            for doc in sortedkeys:
-                # getting doclimit and docoffset for a particular document
-                doclimit = form.getvalue("doc%dlimit" % docnumber)
-                if not doclimit:
-                    doclimit = 3
-                elif round(int(doclimit)) < 0:
-                    doclimit = 0
-                else: doclimit = round(int(doclimit))
-                docoffset = form.getvalue("doc%doffset" % docnumber)
-                if not docoffset:
-                    docoffset = 0
-                elif int(docoffset) < 0:
-                    docoffset = 0
-                else: docoffset = int(docoffset)
-
-                docactiontype = str(form.getvalue("action%d" % docnumber))
-                if docactiontype == "Beginning":
-                    docoffset = 0
-                elif docactiontype == "Forward":
-                    docoffset = docoffset + doclimit
-                elif docactiontype == "Back":
-                    docoffset = docoffset - doclimit
-                
-                docslimoff[docnumber] = [doclimit, docoffset]
-                docnumber += 1
             
-            searchresultlimoff = self.server.search_engine.lim_off_context_window_search(tokenquery,
-                                             limit, offset, docslimoff)
-            sortedkeyslimoff = list(searchresultlimoff.keys())
-            sortedkeyslimoff.sort()
-            for x, doc in enumerate(sortedkeyslimoff):
-                # x = docnumber; docslimoff[x][0] = doclimit; docslimoff[x][1] = docoffset
+            for x, doc in enumerate(sortedkeys):
+                # x = doc number; docslimoff[x][0] = doclimit; docslimoff[x][1] = docoffset
                 self.wfile.write(bytes("<li><p>%s</p></li><ul>" % doc, encoding="UTF-8"))
-                for cw in searchresultlimoff[doc]:
-                    self.wfile.write(bytes("<li><p>%s</p></li>" % cw, encoding="UTF-8"))
+                i = 0
+                for cw in extsearchresult[doc]:
+                    if (i < extdoclimoff[x][0]-1):
+                        self.wfile.write(bytes("<li><p>%s</p></li>" % cw, encoding="UTF-8"))
+                    i += 1
                 self.wfile.write(bytes("</ul>", encoding="utf-8"))
                 self.wfile.write(bytes("""
                     <input type="number" name="doc%dlimit" placeholder = "cit.limit in the doc" value = "%d">
                     <input type="hidden" name="doc%doffset" placeholder = "cit.offset in the doc" value = "%d">
                     <br>
-                    <input type="submit" name="action" value = "Beginning">
-                    """ % (x, docslimoff[x][0], x, docslimoff[x][1]), encoding="utf-8"))
-                if docslimoff[x][1] == 0:
+                    <input type="submit" name="action%d" value = "Beginning">
+                    """ % (x, extdoclimoff[x][0] - 1, x, extdoclimoff[x][1], x), encoding="utf-8"))
+                if extdoclimoff[x][1] < 1:
                     self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Back" disabled>""" % x, encoding="utf-8"))
                 else:
                     self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Back"> """ % x, encoding="utf-8"))
-                intermeddoclimoff = docslimoff
-                intermeddoclimoff[x][1] = intermeddoclimoff[x][1] + intermeddoclimoff[x][0]
-                nextpagesearchresult = self.server.search_engine.lim_off_context_window_search(tokenquery,
-                                                        limit, offset, intermeddoclimoff)
-                if (len(nextpagesearchresult.keys()) > 0 and intermeddoclimoff[x][0] != 0):
-                    self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Forward">""" % x, encoding="utf-8"))
-                else:
+                if ((len(extsearchresult[doc]) < extdoclimoff[x][0]) or (extdoclimoff[x][0] == 0)):
                     self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Forward" disabled>""" % x, encoding="utf-8"))
-                
+                else:
+                    self.wfile.write(bytes("""<input type="submit" name="action%d" value = "Forward">""" % x, encoding="utf-8"))
             self.wfile.write(bytes("""<input type="hidden" name="offset" value = "%d">
                                 </form>
                             </ol>
